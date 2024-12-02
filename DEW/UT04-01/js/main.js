60604c40336b3d88d4248dfa3d30d9cbc66a7ba6
2dawb <2dawb@PC234017>
2024-12-02 09:53:43 +0000

    chore: Update form styling and validation

--- DEW/UT04-01/js/main.js
+++ DEW/UT04-01/js/main.js
@@ -1,3 +1,30 @@
+
+const DOM = {
+    form: document.querySelector('form'),
+    nombre: document.getElementById('nombreUser'),
+    apellido: document.getElementById('apellido'),
+    email: document.getElementById('email'),
+    contra: document.getElementById('contra'),
+    checkpass: document.getElementById('checkpass'),
+    fecha: document.getElementById('fecha'),
+};
+
+DOM.form.addEventListener('submit', (e) => { 
+    if(!DOM.nombre.validationMessage=="") {
+        e.preventDefault()
+        alert(DOM.nombre.validationMessage);
+    }
+    if(!DOM.apellido.validationMessage=="") {
+        e.preventDefault()
+        alert(DOM.apellido.validationMessage);
+    }
+    if(!DOM.email.validationMessage=="") {
+        e.preventDefault()
+        alert(DOM.email.validationMessage);
+    }
+});
+
+
 export function showPassword() {
     const passwordInput = document.getElementById('contra');
     const checkbox = document.getElementById('checkpass');
